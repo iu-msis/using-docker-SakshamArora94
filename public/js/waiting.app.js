34c24391df6d74956576903f101ea62c3cd54315
Saksham Arora <sakshamarora94@gmail.com>
2019-09-14 19:51:57 -0400

    Created Javascript file to hit API and record response to pass it to the HTML page. Linked Javascript file to HTML page

---
+++ public/js/waiting.app.js
@@ -0,0 +1,23 @@
+var waitingApp = new Vue({
+  el: '#patientWaitingApp',
+  data: {
+    patients: []
+  },
+  methods: {             //collection of methods our application can use
+    fetchPatients() {
+      fetch('https://randomuser.me/api/')
+      .then(response => response.json())    //Once a response has been received from the fetch function, only then will this get executed
+      .then(json => {waitingApp.patients = json});
+
+      // Means the same at this
+      // fetch('https://randomuser.me/api/')
+      // .then(function(response) {return response.json()})
+      // .then(function(json) {waitingApp.people = json});
+
+    }
+  },
+  created: function() {       //method called on creaton
+    this.fetchPatients();
+  }
+
+})
